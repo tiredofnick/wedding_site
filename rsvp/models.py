d310f45f5b06a5e6ddcbc86ae56d2ff32d176c8c
tiredofnick <nick.bonadies@gmail.com>
2011-01-24 21:38:33 -0500

    added admin section

--- rsvp/models.py
+++ rsvp/models.py
@@ -1,4 +1,28 @@
 from django.db import models
 
 class Rsvp(models.Model):
+	family = models.CharField(max_length=60)
+	def __unicode__(self):
+	        return self.family
 	
+class Attendee(models.Model):
+	rsvp = models.ForeignKey(Rsvp)
+	name = models.CharField(max_length=60)
+	ATTENDING_CHOICES = (
+		('1', 'Yes!'),
+		('0', 'No, Sorry!'),
+	)
+	attending = models.CharField(max_length=1, choices=ATTENDING_CHOICES)
+	def __unicode__(self):
+	        return self.name
+	
+class Meal(models.Model):
+	attendee = models.ForeignKey(Attendee)
+	MEAL_CHOICES = (
+		('0', 'Clambake'),
+		('1', 'Vegetarian'),
+		('2', 'Chicken')
+	)
+	meal = models.CharField(max_length=1, choices=MEAL_CHOICES)
+	def __unicode__(self):
+	        return self.meal
--- settings.py
+++ settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'getmarried.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -90,7 +90,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'rsvp'
 )
