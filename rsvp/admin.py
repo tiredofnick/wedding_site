8ed9230991e832ccc695d70569b473722770a8a3
tiredofnick <nick.bonadies@gmail.com>
2011-01-26 10:40:09 -0500

    todo: trash meal model

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -3,6 +3,21 @@ from rsvp.models import Attendee
 from rsvp.models import Meal
 from django.contrib import admin
 
-admin.site.register(Rsvp)
-admin.site.register(Attendee)
-admin.site.register(Meal)
+# admin.site.register(Rsvp)
+# admin.site.register(Attendee)
+# admin.site.register(Meal)
+
+class MealInline(admin.StackedInline):
+    model = Meal
+
+class AttendeeInline(admin.ModelAdmin):
+    model = Attendee
+    extra = 3
+
+class RsvpAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['confirmation_code']}),
+    ]
+    inlines = [AttendeeInline]
+
+admin.site.register(Rsvp, RsvpAdmin)
