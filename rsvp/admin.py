bad9299fe488aaacc6479a3b633f561f5687afa0
tiredofnick <nick.bonadies@gmail.com>
2011-01-26 10:15:52 -0500

    added admin page

---
+++ rsvp/admin.py
@@ -0,0 +1,8 @@
+from rsvp.models import Rsvp
+from rsvp.models import Attendee
+from rsvp.models import Meal
+from django.contrib import admin
+
+admin.site.register(Rsvp)
+admin.site.register(Attendee)
+admin.site.register(Meal)
