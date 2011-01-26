92dc0382cdeacf8ef1dfe7dbb2a0ee706d783b8c
tiredofnick <nick.bonadies@gmail.com>
2011-01-26 11:42:58 -0500

    admin is working

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -1,20 +1,12 @@
 from rsvp.models import Rsvp
 from rsvp.models import Attendee
-#from rsvp.models import Meal
 from django.contrib import admin
 
-# admin.site.register(Rsvp)
-# admin.site.register(Attendee)
-# admin.site.register(Meal)
-
-class AttendeeInline(admin.StackedInline):
+class AttendeeInline(admin.TabularInline):
     model = Attendee
     extra = 3
 
 class RsvpAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['confirmation_code']}),
-    ]
     inlines = [AttendeeInline]
 
 admin.site.register(Rsvp, RsvpAdmin)
--- rsvp/models.py
+++ rsvp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Rsvp(models.Model):
 	confirmation_code = models.CharField(max_length=60)
 	def __unicode__(self):
-	        return self.family
+	        return self.confirmation_code
 	
 class Attendee(models.Model):
 	rsvp = models.ForeignKey(Rsvp)
@@ -12,12 +12,10 @@ class Attendee(models.Model):
 		('1', 'Yes!'),
 		('0', 'No, Sorry!'),
 	)
-	attending = models.CharField(null=True, max_length=1, choices=ATTENDING_CHOICES)
+	attending = models.CharField(blank=True, max_length=1, choices=ATTENDING_CHOICES)
 	MEAL_CHOICES = (
 		('0', 'Clambake'),
 		('1', 'Vegetarian'),
 		('2', 'Chicken')
 	)
-	meal = models.CharField(null=True, max_length=1, choices=MEAL_CHOICES)
-	def __unicode__(self):
-	        return self.name
+	meal = models.CharField(blank=True, max_length=1, choices=MEAL_CHOICES)
