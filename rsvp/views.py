bc7d03234a19ba2f7f80d72b37739c0347caecc1
tiredofnick <nick.bonadies@gmail.com>
2011-02-01 15:39:30 -0500

    broken conf submit

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 from rsvp.models import Rsvp
 
-def index(request):
-	return render_to_response('rsvp/index.html')
 
-def detail(request, rsvp_id):
-	r = get_object_or_404(Rsvp, confirmation_code=get(request.POST['confirmation_code']))
-	return render_to_response('rsvp/detail.html', {'rsvp': r}, context_instance=RequestContext(r))
+def index(request):
+	return render_to_response('rsvp/index.html', context_instance=RequestContext(request))
+
+def detail(request):
+	# r = get_object_or_404(Rsvp, confirmation_code=get(request.POST['confirmation_code']))
+	try:
+		r = request.POST['confirmation_code']
+		return render_to_response('rsvp/detail.html', {'rsvp': r})
