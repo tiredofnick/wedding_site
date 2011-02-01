87261f1491dec8f693e4c3a18e48be4c94338cce
tiredofnick <nick.bonadies@gmail.com>
2011-02-01 13:53:21 -0500

    moved files around, created proper template directory

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^rsvp/$', 'rsvp.views.index'),
-	(r'^rsvp/(?P<rsvp_id>\d+)/$', 'rsvp.views.detail'),
+    (r'^$', 'rsvp.views.index'),
+	(r'^(?P<rsvp_id>\d+)/$', 'rsvp.views.detail'),
 )
--- urls.py
+++ urls.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^rsvp/', include(rsvp.urls)),
+    (r'^rsvp/', include('rsvp.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
