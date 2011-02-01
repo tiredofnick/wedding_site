f1ce824fcfad264ffec5cdf5cef01584c4dedcc8
tiredofnick <nick.bonadies@gmail.com>
2011-02-01 14:17:38 -0500

    fixed urls

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'rsvp.views.index'),
-	(r'^(?P<rsvp_id>\d+)/$', 'rsvp.views.detail'),
+	(r'^confirm/(?P<rsvp_id>\d+)/$', 'rsvp.views.detail'),
 )
