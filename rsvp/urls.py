54ec4bc2de55f0c37c6dcab093a61f8c248a6d1d
tiredofnick <nick.bonadies@gmail.com>
2011-02-01 15:03:47 -0500

    broken conf submit

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'rsvp.views.index'),
+	(r'^confirm/$', 'rsvp.views.detail'),
 	(r'^confirm/(?P<rsvp_id>\d+)/$', 'rsvp.views.detail'),
 )
--- rsvp/views.py
+++ rsvp/views.py
@@ -5,5 +5,5 @@ def index(request):
 	return render_to_response('rsvp/index.html')
 
 def detail(request, rsvp_id):
-	r = get_object_or_404(Rsvp, confirmation_code=rsvp_id)
-	return render_to_response('rsvp/detail.html', {'rsvp': r})
+	r = get_object_or_404(Rsvp, confirmation_code=get(request.POST['confirmation_code']))
+	return render_to_response('rsvp/detail.html', {'rsvp': r}, context_instance=RequestContext(r))
