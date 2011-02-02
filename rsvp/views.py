ca7604943d1d170f0fa4153eec682e8151610e44
tiredofnick <nick.bonadies@gmail.com>
2011-02-02 17:26:10 -0500

    more messing with the views, forms still not totally right.

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
-from rsvp.models import Rsvp, Attendee
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from rsvp.models import Rsvp, Attendee, RsvpForm
 
 
 def index(request):
-	return render_to_response('rsvp/index.html', context_instance=RequestContext(request))
+	#return render_to_response('rsvp/index.html', context_instance=RequestContext(request))
+	if request.method == 'POST':
+		rsvp = RsvpForm(request.POST)
+		try:
+			r = Rsvp.objects.get(confirmation_code=request.POST['confirmation_code'])
+		except Rsvp.DoesNotExist:
+			raise Http404
+		return render_to_response('rsvp/detail.html', {'rsvp': r})
+	else:
+		return render_to_response('rsvp/index.html', context_instance=RequestContext(request))
 
-def detail(request):
-	r = get_object_or_404(Rsvp, confirmation_code=request.POST['confirmation_code'])
+def detail(request, code):
+	r = get_object_or_404(Rsvp, confirmation_code=code)
+#	r = get_object_or_404(Rsvp, confirmation_code=request.POST['confirmation_code'])
 	return render_to_response('rsvp/detail.html', {'rsvp': r})
 	#try:
 	#	r = request.POST['confirmation_code']
