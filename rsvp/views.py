f47e2c5a6747e4f0e5eddd3d2e73be12a1214f9f
tiredofnick <nick.bonadies@gmail.com>
2011-02-02 11:43:02 -0500

    getting detail view working

--- rsvp/views.py
+++ rsvp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
-from rsvp.models import Rsvp
+from rsvp.models import Rsvp, Attendee
 
 
 def index(request):
